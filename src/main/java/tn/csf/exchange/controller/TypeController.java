1b9e5ea4a3b5f37533b5270540b6de220006b5bb
houdaguirat <gkhouda84@gmail.com>
2023-04-11 16:55:41 +0200

    commit puch

--- src/main/java/tn/csf/exchange/controller/TypeController.java
+++ src/main/java/tn/csf/exchange/controller/TypeController.java
@@ -7,52 +7,53 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;  
 import org.springframework.web.bind.annotation.PathVariable;  
 import org.springframework.web.bind.annotation.PostMapping;  
-import org.springframework.web.bind.annotation.PutMapping;  
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import tn.csf.exchange.model.Type;
+import tn.csf.exchange.service.TypeService;
 
 
 
 
-
+@RestController
 //mark class as Controller  
-@RestController 
+
 @CrossOrigin(origins = "*")
 
 public class TypeController {
 
 	//autowire the TypeService class  
 	@Autowired  
-	TypeController tc;  
+	TypeService ts;  
 
 	//creating a get mapping that retrieves all the Type detail from the database   
 	@GetMapping("/Type")
 	private List<Type> getAllTypes()   
 	{  
-		return tc.getAllTypes();  
+		return ts.getAllTypes();  
 	}  
 
 	//creating a get mapping that retrieves the detail of a specific Type  
 	@GetMapping("/Type/{id}")  
 	private Type getType(@PathVariable("id") int id)   
 	{  
-		return tc.getType(id);  
+		return ts.getTypeById(id);  
 	}  
 
 	//creating a delete mapping that deletes a specified Type  
 	@DeleteMapping("/Type/{id}")  
 	private void deleteType(@PathVariable("id") int id)   
 	{  
-		tc.deleteType(id);
+		ts.delete(id);
 	} 
 
 	//create new Type
 	@PostMapping("/Type")  
 	private int saveType(@RequestBody Type p)   
 	{  
-		tc.update(p);  
+		ts.delete(0);  
 		return p.getId();  
 	} 
 
@@ -60,7 +61,7 @@ public class TypeController {
 	@PutMapping("/Type")  
 	private Type update(@RequestBody  Type p)   
 	{  
-		tc.update(p);  
+		ts.delete(0);  
 		return p;  
 	}  
 }
